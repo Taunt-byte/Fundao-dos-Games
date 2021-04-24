659dd84168a4ab278a200f885ab358b6d1e0c176
lucas <lucasluan94@hotmail.com>
2021-04-23 21:28:12 -0300

    1.5

--- JavaFiles/func.js
+++ JavaFiles/func.js
@@ -1,3 +1,21 @@
-    const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
+var MAX_WIDTH = 3000;
+
+function ScaleSlider() {
+  var containerElement = jssor_1_slider.$Elmt.parentNode;
+  var containerWidth = containerElement.clientWidth;
+
+  if (containerWidth) {
+
+    var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+    jssor_1_slider.$ScaleWidth(expectedWidth);
+  } else {
+    window.setTimeout(ScaleSlider, 30);
   }
+}
+
+ScaleSlider();
+
+$Jssor$.$AddEvent(window, "load", ScaleSlider);
+$Jssor$.$AddEvent(window, "resize", ScaleSlider);
+$Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
